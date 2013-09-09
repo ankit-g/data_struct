c31c626835469abb4e1b712cfc6230d380d1cde3
ankit-g <ankitgupta.work@gmail.com>
2013-09-09 13:18:14 +0530

    Short Description of files.

--- include/sll.h
+++ include/sll.h
@@ -4,8 +4,10 @@ struct node
 	struct node *next;
 };
 
+// Provide pointer to head, pointer to data and size of the element.
 extern int add_node(struct node **, void *, int );
 
 extern void free_list(struct node *);
 
+// Provide pointer head and the location of the node.
 extern void *ret_ele(struct node *, int );
